53ea8c6272dc75e553d67d3c0eaea1b09930a76e
jkmacielbr <86507721+jkmacielbr@users.noreply.github.com>
2022-10-08 17:20:41 -0400

    Primeiro Formulário com API

---
+++ Formulário Cep/app.js
@@ -0,0 +1,24 @@
+function buscarCep(){
+    const cep = document.querySelector("#cep").value;
+    const url = 'https://brasilapi.com.br/api/cep/v2/'+cep;
+
+    fetch(url).then(response => response.json()).then(response =>{ console.log(response)
+        
+
+
+        if(response.name =='CepPromiseError'){
+            document.getElementById('estado').removeAttribute('readonly')
+            document.getElementById('cidade').removeAttribute('readonly')
+            document.getElementById('rua').removeAttribute('readonly')
+            document.getElementById('bairro').removeAttribute('readonly')
+        } else{
+            document.getElementById('estado').value=response.state;
+            document.getElementById('cidade').value=response.city;
+            document.getElementById('rua').value=response.street;
+            document.getElementById('bairro').value=response.neighborhood;
+
+        }
+
+})
+}
+
